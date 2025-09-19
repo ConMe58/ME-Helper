8faa0d0cb81f2cbdbe8898d25eafed8dc4cc957d
ConMe58 <yogi.seesack.0m@icloud.com>
2025-09-19 19:55:01 +0200

    Update and rename tiles.json to tiles.js

--- tiles.js
+++ tiles.js
@@ -1,4 +1,4 @@
-[
+const tilesData = [
   {"id": "pause", "label": "Pause", "sentence": "Gerade ist es zu viel, ich brauche eine Pause."},
   {"id": "trinken", "label": "Trinken", "sentence": "Ich habe Durst und muss was trinken."},
   {"id": "wc", "label": "WC", "sentence": "Ich muss auf Toilette."},
@@ -8,4 +8,4 @@
   {"id": "schmerzen", "label": "Schmerzen", "sentence": "Ich habe gerade starke Schmerzen."},
   {"id": "crash", "label": "Crash", "sentence": "Ich bin gerade in einem starken Crash."},
   {"id": "nicht", "label": "Nicht sprechen", "sentence": "Bitte jetzt nicht mit mir sprechen."}
-]
+];
